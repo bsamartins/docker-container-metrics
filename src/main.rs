dad32d80d015a775c1675d88e7c57fb3a5480375
Bernardo Martins <bernardo.martins@utmost.co>
2024-10-15 15:27:48 +0100

    Expose metrics

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use bollard::container::{ListContainersOptions, NetworkStats, Stats, StatsOptions};
+use bollard::container::{ListContainersOptions, Stats, StatsOptions};
 use bollard::errors::Error;
 use bollard::models::ContainerSummary;
 use bollard::Docker;
 use futures_util::stream::StreamExt;
 use futures_util::Stream;
-use metrics::{counter, describe_counter, describe_gauge, describe_histogram, gauge, histogram};
+use metrics::gauge;
 use metrics_exporter_prometheus::PrometheusBuilder;
 use std::collections::HashMap;
-use std::net::{Ipv4Addr, SocketAddrV4};
 
 #[tokio::main]
 async fn main() {
@@ -27,19 +26,15 @@ async fn main() {
 
     let mut stats_stream = container_stats(docker, container_names);
 
-    describe_gauge!("container_network_rx_bytes", "Description for container_network_rx_bytes");
-    describe_gauge!("container_network_tx_bytes", "Description for container_network_tx_bytes");
-
     while let Some(stats_result) = stats_stream.next().await {
         match stats_result {
             Ok(stats) => {
-                let name_label = ("name", stats.name.to_string());
-                gauge!("container_network_rx_bytes", "name" => "test").increment(1.0);
-                println!("{}", stats.name);
+                let container_name = stats.name.replace("/", "");
+                let container_name_label = ("name", container_name.clone());
                 match stats.networks {
                     Some(networks) => {
                         networks.iter().for_each(|(network, net_stats)| {
-                            let network_labels = &[name_label.clone(), ("network", network.to_string())];
+                            let network_labels = &[container_name_label.clone(), ("network", network.to_string())];
                             gauge!("container_network_rx_bytes", network_labels).set(net_stats.rx_bytes as f64);
                             gauge!("container_network_tx_bytes", network_labels).set(net_stats.tx_bytes as f64);
                             gauge!("container_network_rx_packets", network_labels).set(net_stats.rx_packets as f64);
@@ -63,10 +58,10 @@ fn container_stats(docker: Docker, container_names: Vec<String>) -> impl Stream<
     };
 
     let streams = container_names.iter().map(|container_name| {
-        println!("container name: {}", container_name);
         docker.stats(container_name, Some(stats_options))
     }).collect::<Vec<_>>();
-    ::futures::stream::iter(streams).flatten()
+
+    futures::stream::select_all(streams)
 }
 
 async fn list_containers(docker: &Docker) -> Vec<ContainerSummary> {
